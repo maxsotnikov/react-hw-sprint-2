fba688911b2460ff8db3c68b926ee3c2ea835126
Max Sotnikov <maxsotnikov00@gmail.com>
2026-01-20 20:31:47 +0300

    hw7

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,6 +37,8 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
+      onChangeOption && onChangeOption(Number(e.currentTarget.value))
+      onChange && onChange(e)
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,6 +36,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+      onChangeOption && onChangeOption(Number(e.currentTarget.value))
+      onChange && onChange(e)
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,6 +50,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                      name={name}
+                      value={o.id}
+                      checked={value === o.id}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
