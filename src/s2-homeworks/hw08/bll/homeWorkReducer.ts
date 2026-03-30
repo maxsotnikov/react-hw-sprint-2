b1951d3d986ac4f9f297504708c16390247e7018
Max Sotnikov <maxsotnikov00@gmail.com>
2026-03-30 18:11:34 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,20 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      if (action.payload === 'up') return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+      if (action.payload === 'down') return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+      return state // need to fix
     }
+    case 'check': {
+      return state.filter(u => u.age >= action.payload) // need to fix
+    }
+    default:
+      return state
+  }
 }
